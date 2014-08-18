dbc901a4babfbda054c2de6f9b7069f0fcdd7fcf
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-08-18 16:44:08 -0300

    zenity

--- utils/srt.go
+++ utils/srt.go
@@ -4,11 +4,24 @@ import (
 	"fmt"
 	"github.com/sloonz/go-iconv"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 	"regexp"
 	"strings"
 )
+
+func showInfo(s string) {
+	fmt.Print(s)
+	cmd := exec.Command("zenity", "--info", "--text="+s)
+	cmd.Run()
+}
+
+func showError(s string) {
+	cmd := exec.Command("zenity", "--error", "--text="+s)
+	cmd.Run()
+	log.Fatal(s)
+}
 
 func main() {
 
@@ -21,17 +34,17 @@ func main() {
 
 	out, err := cmd.Output()
 	if err != nil {
-		os.Exit(1)
+		showError(fmt.Sprintf("[%s] error=[%s].\n", os.Args[1], err))
 	}
 
 	charset := strings.Split(string(out), "=")
 	if len(charset) != 2 {
-		os.Exit(1)
+		showError(fmt.Sprintf("[%s] charset error=[%s].\n", os.Args[1], charset))
 	}
 
 	content, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
-		os.Exit(1)
+		showError(fmt.Sprintf("[%s] error=[%s].\n", os.Args[1], err))
 	}
 
 	text, err := iconv.Conv(string(content), "UTF-8", charset[1])
@@ -42,9 +55,9 @@ func main() {
 	sub := regexp.MustCompile("(?i)<.*?i>|(?i)<.*?b>|(?i)<.*?u>").ReplaceAllString(text, "")
 
 	if err := ioutil.WriteFile(os.Args[1], []byte(sub), 0644); err != nil {
-		fmt.Printf("[%s] error=[%s].\n", os.Args[1], err)
-		os.Exit(1)
+		e := fmt.Sprintf("[%s] error=[%s].\n", os.Args[1], err)
+		showError(e)
 	}
 
-	fmt.Printf("[%s] ok.\n", os.Args[1])
+	showInfo(fmt.Sprintf("[%s] ok.\n", os.Args[1]))
 }
