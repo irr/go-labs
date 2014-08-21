b11a0f2d43f9f1235ad3dd3b906d264d5c83a29d
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-08-21 18:28:52 -0300

    binheap

--- algo/binheap.go
+++ algo/binheap.go
@@ -64,22 +64,51 @@ func (b *BinHeap) delmin() (int, bool) {
 	return r, true
 }
 
+func BuildHeap(alist []int) *BinHeap {
+	if alist == nil || len(alist) == 0 {
+		return NewBinHeap()
+	}
+	b := NewBinHeap()
+	i := len(alist) / 2
+	b.size = len(alist)
+	b.list = append(alist, 0)
+	copy(b.list[1:], alist[0:])
+	b.list[0] = 0
+	for i > 0 {
+		b.percDown(i)
+		i--
+	}
+	return b
+}
+
 func main() {
 	b := NewBinHeap()
 	fmt.Printf("%#v\n", b)
+
 	v, ok := b.delmin()
 	fmt.Printf("%#v[%v]\n", v, ok)
+
 	for _, v := range []int{33, 17, 27, 18, 14, 9, 19, 21, 11, 5, 7} {
 		b.insert(v)
 		fmt.Printf("%#v\n", b)
 	}
+
 	for i := 0; i < 3; i++ {
 		v, ok := b.delmin()
 		fmt.Printf("%#v[%v]\n", v, ok)
 		fmt.Printf("%#v\n", b)
 	}
+
 	for _, v := range []int{1, 100} {
 		b.insert(v)
 		fmt.Printf("%#v\n", b)
+	}
+
+	bh := BuildHeap([]int{33, 17, 27, 18, 14, 9, 19, 21, 11, 5, 7})
+	fmt.Printf("%#v\n", bh)
+	for i := 0; i < 11; i++ {
+		v, ok := bh.delmin()
+		fmt.Printf("%#v[%v]\n", v, ok)
+		fmt.Printf("%#v\n", bh)
 	}
 }
