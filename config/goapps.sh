a687a6de32a5058f8a5fe9aa80b414321f5c637c
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-01-16 10:09:19 -0200

    ubuntu-14.04.1

---
+++ config/goapps.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo mkdir -p /opt/goapps
+sudo chown irocha: /opt/goapps
+cd /opt/goapps
+curl -L  https://github.com/coreos/etcd/releases/download/v2.0.0-rc.1/etcd-v2.0.0-rc.1-linux-amd64.tar.gz -o etcd-v2.0.0-rc.1-linux-amd64.tar.gz
+tar xfva etcd-v2.0.0-rc.1-linux-amd64.tar.gz
+rm -rf etcd; ln -s etcd-v2.0.0-rc.1-linux-amd64 etcd
+sudo ln -s /opt/goapps/etcd/etcd /usr/local/bin/etcd
+sudo ln -s /opt/goapps/etcd/etcd /usr/local/bin/etcdctl
+sudo ln -s /opt/goapps/etcd/etcd /usr/local/bin/etcd-migrate
+wget https://s3.amazonaws.com/bitly-downloads/nsq/nsq-0.3.0.linux-amd64.go1.3.3.tar.gz
+tar xfva nsq-0.3.0.linux-amd64.go1.3.3.tar.gz
+rm -rf nsq;ln -s nsq-0.3.0.linux-amd64.go1.3.3 nsq
+sudo ln -s /opt/goapps/
+sudo ln -s /opt/goapps/nsq/bin/nsqadmin /usr/local/bin/nsqadmin
+sudo ln -s /opt/goapps/nsq/bin/nsqd /usr/local/bin/nsqd
+sudo ln -s /opt/goapps/nsq/bin/nsqlookupd /usr/local/bin/nsqlookupd
+sudo ln -s /opt/goapps/nsq/bin/nsq_pubsub /usr/local/bin/nsq_pubsub
+sudo ln -s /opt/goapps/nsq/bin/nsq_stat /usr/local/bin/nsq_stat
+sudo ln -s /opt/goapps/nsq/bin/nsq_tail /usr/local/bin/nsq_tail
+sudo ln -s /opt/goapps/nsq/bin/nsq_to_file /usr/local/bin/nsq_to_file
+sudo ln -s /opt/goapps/nsq/bin/nsq_to_http /usr/local/bin/nsq_to_http
+sudo ln -s /opt/goapps/nsq/bin/nsq_to_nsq /usr/local/bin/nsq_to_nsq
