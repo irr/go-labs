ad7c733960555ec08671163b9e533935389b92bd
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-04-22 14:18:21 -0300

    update

--- config/goapps.sh
+++ config/goapps.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+sudo rm -rf /opt/goapps /usr/local/bin/etcd* /usr/local/bin/nsq*
 sudo mkdir -p /opt/goapps
 sudo chown irocha: /opt/goapps
 cd /opt/goapps
-curl -L  https://github.com/coreos/etcd/releases/download/v2.0.0-rc.1/etcd-v2.0.0-rc.1-linux-amd64.tar.gz -o etcd-v2.0.0-rc.1-linux-amd64.tar.gz
-tar xfva etcd-v2.0.0-rc.1-linux-amd64.tar.gz
-rm -rf etcd; ln -s etcd-v2.0.0-rc.1-linux-amd64 etcd
+# https://github.com/coreos/etcd/releases/
+curl -L  https://github.com/coreos/etcd/releases/download/v2.0.9/etcd-v2.0.9-linux-amd64.tar.gz -o etcd-v2.0.9-linux-amd64.tar.gz
+tar xzvf etcd-v2.0.9-linux-amd64.tar.gz
+rm -rf etcd; ln -s etcd-v2.0.9-linux-amd64 etcd
 sudo ln -s /opt/goapps/etcd/etcd /usr/local/bin/etcd
 sudo ln -s /opt/goapps/etcd/etcd /usr/local/bin/etcdctl
 sudo ln -s /opt/goapps/etcd/etcd /usr/local/bin/etcd-migrate
-wget https://s3.amazonaws.com/bitly-downloads/nsq/nsq-0.3.0.linux-amd64.go1.3.3.tar.gz
-tar xfva nsq-0.3.0.linux-amd64.go1.3.3.tar.gz
-rm -rf nsq;ln -s nsq-0.3.0.linux-amd64.go1.3.3 nsq
+# http://nsq.io/deployment/installing.html
+wget https://s3.amazonaws.com/bitly-downloads/nsq/nsq-0.3.2.linux-amd64.go1.4.1.tar.gz
+tar xfva nsq-0.3.2.linux-amd64.go1.4.1.tar.gz
+rm -rf nsq;ln -s nsq-0.3.2.linux-amd64.go1.4.1 nsq
 sudo ln -s /opt/goapps/
 sudo ln -s /opt/goapps/nsq/bin/nsqadmin /usr/local/bin/nsqadmin
 sudo ln -s /opt/goapps/nsq/bin/nsqd /usr/local/bin/nsqd
