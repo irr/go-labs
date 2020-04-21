6e93f1c9d187149523f9378342f26992960fdcca
Ivan Ribeiro Rocha <ivan.ribeiro@gmail.com>
2020-04-21 10:54:48 +0100

    pprof

--- router-lookup/go.mod
+++ router-lookup/go.mod
@@ -8,4 +8,6 @@ require (
 	github.com/gorilla/mux v1.7.4 // indirect
 	github.com/gorilla/pat v1.0.1 // indirect
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/pborman/getopt v0.0.0-20190409184431-ee0cd42419d3 // indirect
 )
--- router-lookup/main.go
+++ router-lookup/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"os"
+	"runtime/pprof"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/oklog/ulid"
 )
 
 func httprouterHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -15,28 +22,44 @@ func httprouterHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Par
 	fmt.Fprintf(w, "id: %v and nid: %v\n", id, nid)
 }
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	mux := httprouter.New()
 	mux.GET("/other/:id/endpoint/:nid", httprouterHandler)
 	mux.GET("/other/:id/endpoint/:nid/", httprouterHandler)
 
-	uri := "/other/7b2a774e-6d59-11ea-90c6-1f5c859414bc/endpoint/100?q=mytest"
+	entropy := ulid.Monotonic(rand.New(rand.NewSource(time.Now().UnixNano())), 0)
 
-	u, err := url.Parse(uri)
-	if err != nil {
-		panic(err)
-	}
+	for i := 1; i < 10000; i++ {
+		uri := fmt.Sprintf("/other/%v/endpoint/100?q=mytest", ulid.MustNew(ulid.Timestamp(time.Now()), entropy))
 
-	fmt.Printf(" u:%+v\n", u.Path)
+		u, err := url.Parse(uri)
+		if err != nil {
+			panic(err)
+		}
 
-	w := httptest.NewRecorder()
-	h, p, ok := mux.Lookup("GET", u.Path)
+		fmt.Printf(" u:%+v\n", u.Path)
 
-	if h != nil {
-		fmt.Printf(" h: %+v\n p: %+v\nok: %+v\n", h, p, ok)
+		w := httptest.NewRecorder()
+		h, p, ok := mux.Lookup("GET", u.Path)
 
-		h(w, nil, p)
+		if h != nil {
+			fmt.Printf(" h: %+v\n p: %+v\nok: %+v\n", h, p, ok)
 
-		fmt.Printf("%+v\n", w.Body.String())
+			h(w, nil, p)
+
+			fmt.Printf("%+v\n", w.Body.String())
+		}
 	}
 }
